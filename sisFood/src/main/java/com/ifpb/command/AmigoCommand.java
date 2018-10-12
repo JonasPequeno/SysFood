2f0545dd138ad44d40c4a2ab6758837aec97f293
Jonas Pequeno <Jonaspdasilva31@gmail.com>
2018-10-12 16:51:48 -0300

    criado gereciador e command de amigos, implementado metodos de listagem de amigos

---
+++ sisFood/src/main/java/com/ifpb/command/AmigoCommand.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ifpb.command;
+
+import com.ifpb.interfaces.CommandIF;
+import com.ifpb.model.Manager.AmigosManager;
+import com.ifpb.model.Usuario;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author jonas
+ */
+public class AmigoCommand implements CommandIF{
+
+    @Override
+    public void execute(HttpServletRequest request, HttpServletResponse response) throws SQLException, ClassNotFoundException, IOException, ServletException {
+        
+        HttpSession sessao = request.getSession();
+        
+        AmigosManager gereciador = new AmigosManager();
+        
+        Object user = request.getSession().getAttribute("Usuario");
+        Usuario usuario = (Usuario) (user);
+        
+        List<String> amigos = new ArrayList<>();
+            amigos = gereciador.listarAmigos(usuario.getEmail());
+        
+        request.getSession().setAttribute("listaAmigos",amigos);
+        
+    }
+    
+}
---
+++ sisFood/src/main/java/com/ifpb/model/Manager/AmigosManager.java
@@ -0,0 +1,47 @@
+package com.ifpb.model.Manager;
+
+import com.ifpb.factory.DaoFactory;
+import com.ifpb.interfaces.AmigosIF;
+import com.ifpb.interfaces.DaoFactoryIF;
+import java.util.List;
+
+/**
+ *
+ * @author jonas
+ */
+public class AmigosManager  implements  AmigosIF{
+    
+    private DaoFactoryIF factory = null;
+    private AmigosIF amigosDao = null;
+
+    public AmigosManager() {
+        factory = DaoFactory.createFactoryDao();
+        amigosDao = factory.criaAmigoDao();
+    }
+    
+    @Override
+    public List<String> listarAmigos(String email) {
+        return amigosDao.listarAmigos(email);
+    }
+
+    @Override
+    public boolean aceitarAmizade(String userRemetente, String userDestinatario) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean excluirAmigo(String userRemetente, String userDestinatario) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean enviarSolicitacao(String userRemetente, String userDestinatario) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean recusarSolicitacao(String userRemtente, String userDestinatario) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
